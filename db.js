c0cf3a2234f5c8321a18cb7e59fe7e8f76c72e07
Arez Shahid <arezmaker@gmail.com>
2023-05-03 19:35:36 +0500

    Reviews Done

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToMongo = () =>
 {
-    mongoose.connect("mongodb://localhost:27017/techmedic", () =>
+    mongoose.connect("mongodb://127.0.0.1:27017/techmedic", () =>
     {
         console.log("Connected to DB");
     })
--- routes/reviews.js
+++ routes/reviews.js
@@ -1,13 +1,38 @@
 const express = require('express');
 const User = require('../models/Users');
+const Product = require('../models/Products');
 const router = express.Router();
 const Reviews = require('../models/Reviews');
-let getUser;
-const {body, validationResult} = require('express-router');
+const getAdmin = require('../middleware/getAdmin');
+const {body, validationResult} = require('express-validator');
 
-router.get('/', (req, res)=>{
-    console.log(req.body);
-    res.send(req.body);
+router.post('/addReview', getAdmin, 
+[
+    body('review', 'Enter Review').isLength({min: 1}),
+    body('rating', 'Rate out of 5').isLength({min: 1}),
+], async(req, res) =>
+{
+    const errors = validationResult(req);
+    const {productid, review, rating} = req.body;
+    const user = await User.findById(req.user);
+    const product = await Product.findById(productid);
+    
+    if (!errors.isEmpty())
+    {
+        return res.status(400).json({error: errors.array()});
+    }
+
+    console.log(user);
+    console.log(product);
+    const Review = await Reviews.create({User:user, Product:product, Review:review, Rating:rating});
+    return res.json(Review);
+});
+
+router.get('/getReview/:productid', getAdmin, async (req, res)=>{
+    const {productid} = req.params;
+    let reviews = await Reviews.find();
+    reviews = reviews.length ? reviews.filter(r => r.Product.toString() === productid.toString()) : reviews;
+    return reviews.length ? res.json({reviews}) : res.json({error: 'Be the first to add a review'});
 })
 
 module.exports = router;
